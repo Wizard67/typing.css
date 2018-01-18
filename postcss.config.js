53dea4dd53889d32737f6dbc423981cb1c8bb2ac
Wizard67 <wizard67.hjn@gmail.com>
2018-01-18 13:59:30 +0800

    [modify] 添加 PostCSS 处理样式文件

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+        require('autoprefixer')({
+            browsers: [
+                'last 2 versions',
+                'ie 9',
+            ],
+            flexbox: "no-2009",
+            remove: true,
+        }),
+        require('cssnano')({
+            preset: 'default',
+        }),
+    ]
+}
